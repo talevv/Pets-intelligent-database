26562ac7d18d0c241a2ed9396bcfd384abbbf49e
NekoBard <tadeusz.m.lewandowski@gmail.com>
2016-12-29 20:54:05 +0100

    Add polish speech recognition, add prolog db

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const Prolog = require('jsprolog');
 
 app.use(express.static('public'));
 
@@ -8,9 +9,35 @@ app.get('/', function (req, res) {
 });
 
 app.get('/api/sentence/:sentence', function (req, res) {
-   res.send(req.params.sentence);
+   // res.send(req.params.sentence);
+   res.send(recognizeSentence(req.params.sentence));
 });
 
+// console.log("parser")
+// console.log(Object.keys(Prolog))
+// console.log(Prolog.default.Parser)
+const db = Prolog.default.Parser.parse("cat(mruczek). cat(lohn). cat(heniek).");
+
+const query = Prolog.default.Parser.parseQuery("cat(mruzek).");
+// const iter = Prolog.default.Solver.query(db, query);
+const iter = Prolog.default.Solver.query(db, query);
+
+function recognizeSentence(sentence){
+
+	
+	var result = /^k/.test(sentence);
+
+
+	return result;
+}
+
+// console.log("dwq",recognizeSentence("d"))
+
+// console.log(iter.next())
+// while(iter.next()){
+//     // console.log("X = ", iter.current.X);
+//     console.log(iter);
+// }
 
 const server = app.listen(4000, function () {
    const host = server.address().address
